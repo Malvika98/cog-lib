fbf38b4fb9cfef3cf11f419700e78ec93f89003f
Malvika98 <malavikabalakrishnan2408@gmail.com>
2021-11-09 23:46:27 -0800

    updated 'style.css'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
+import About from "./components/About";
 import "./style.css";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <About/>
     </div>
-  );
-}
+  );}
