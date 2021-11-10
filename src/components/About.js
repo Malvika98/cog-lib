0cf2cab66f4550570f0a59eabc8c01e6abc92e3c
Malvika98 <malavikabalakrishnan2408@gmail.com>
2021-11-09 23:30:27 -0800

    added 'About.js' component

---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="titles">
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus
+        ornare purus quis tincidunt. Vestibulum eleifend, lorem id elementum
+        lobortis, sem orci ullamcorper est, id pellentesque orci neque eget
+        erat. Curabitur eget mi eu urna mattis venenatis quis sit amet lectus.
+        Etiam nec augue ultricies quam tempus facilisis in a odio. Nulla vitae
+        augue risus. Donec vitae risus tristique, fringilla orci nec, dapibus
+        sapien. Aliquam erat volutpat. Etiam dui enim, ornare vel est ut,
+        vulputate rhoncus tellus. Morbi suscipit elit ut risus posuere
+        consectetur sit amet id arcu. Pellentesque ultricies orci enim, vel
+        dignissim enim volutpat quis. Aenean vel volutpat turpis, sed iaculis
+        odio. Fusce tempor magna non facilisis dictum. In odio felis, feugiat eu
+        ultricies in, placerat ut turpis. Praesent volutpat ornare aliquet.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </p>
+      <p>
+        Donec consequat risus et varius rhoncus. Phasellus euismod tortor ex,
+        vel vehicula elit fermentum nec. Nullam vel felis arcu. Nullam magna
+        nisl, venenatis vitae neque at, malesuada laoreet augue. Suspendisse at
+        mattis velit. Sed vestibulum ex in tortor vehicula dapibus. Lorem ipsum
+        dolor sit amet, consectetur adipiscing elit. Vivamus quis imperdiet sem.
+      </p>
+    </div>
+  );
+};
+
+export default About;
